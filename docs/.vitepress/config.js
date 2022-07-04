50cc59e3407e58c3b8f6c5c33a8836724ec75a34
zqm02 <zqm556194@gmail.com>
2022-07-04 23:09:33 +0800

    '初始化提交'

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,79 @@
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+module.exports = {
+  title: "我的笔记", // 顶部左侧标题
+  head: [
+    // 设置 描述 和 关键词
+    [
+      "meta",
+      {
+        name: "keywords",
+        content: "我的笔记 一起学习 面试 面试经历 面经 ",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "description",
+        content: "此文档主要用于记录个人学习笔记",
+      },
+    ],
+  ],
+  // 项目的根路径
+  base: "/my-note/",
+  // 语言
+  lang: "zh-CN",
+
+  markdown: {
+    theme: "vitesse-light",
+    lineNumbers: true,
+  },
+  themeConfig: {
+    // 顶部导航
+    nav: [
+      {
+        text: "JQ学习",
+        link: "/jq/jq1.html",
+        activeMatch: "/jq/",
+      },
+      {
+        text: "Vue2学习",
+        link: "/vue/vue1.html",
+        activeMatch: "/vue/",
+      },
+    ],
+    // socialLinks: [
+    //   { icon: "github", link: "https://github.com/cll123456/common-study" },
+    // ],
+    // 编辑链接
+    // editLink: {
+    //   pattern:
+    //     "https://github.com/cll123456/common-study/edit/master/docs/docs/:path",
+    //   text: "前往GitHub编辑此页",
+    // },
+    // 顶部导航
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present zqm & my-note Contributors",
+    },
+    // 上次编辑的日期
+    lastUpdatedText: "上次更新",
+    // 侧边栏
+    sidebar: [
+      {
+        text: "JQ学习",
+        collapsible: true,
+        items: [
+          { text: "JQ的第一课堂", link: "/jq/jq1.md" },
+          { text: "JQ的第二课堂", link: "/jq/jq2.md" },
+        ],
+      },
+      {
+        text: "Vue2学习",
+        collapsible: true,
+        items: [{ text: "vue2的第一课堂", link: "/vue/vue1.md" }],
+      },
+    ],
+  },
+};
