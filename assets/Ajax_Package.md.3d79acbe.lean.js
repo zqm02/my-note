2ee95c6f7d33dbac8c37de1865aed31be1caf4aa
zqm02 <zqm556194@gmail.com>
2022-07-05 15:48:22 +0800

    deploy: 自动部署

---
+++ assets/Ajax_Package.md.3d79acbe.lean.js
@@ -0,0 +1,40 @@
+import{_ as n,c as t,o as a,a as e}from"./app.e711c1f7.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"Ajax/Package.md"}'),s={name:"Ajax/Package.md"},r=e("pre",null,[e("code",null,`function Ajax_get(){
+    const xhr = new XMLHttpRequest;
+    xhr.open("GET","url",ture);
+     const headers = {
+      "content-type": "application/json",
+    };
+    Object.keys(headers).forEach((key) =>
+      xhr.setRequestHeader(key, headers[key])
+    );
+
+    xhr.send(JSON.stringify());
+    xhr.onreadystatechange = function(){
+        if(onreadystatechange === 4){
+            if(xhr.status >= 200 && xhr.status < 300){
+                \u5904\u7406\u7ED3\u679C
+            }
+        }
+    }
+}
+
+
+function Ajax_post(){
+    const xhr = new XMLHttpRequest;
+    xhr.open("POST","url",true);
+      const headers = {
+      "content-type": "application/json",
+    };
+    Object.keys(headers).forEach((key) =>
+      xhr.setRequestHeader(key, headers[key])
+    );
+    xhr.send(JSON.stringify());
+    xhr.onreadystatechange = function(){
+        if(onreadystatechange === 4){
+            if(xhr.status >= 200 && xhr.status < 300){
+                \u5904\u7406\u7ED3\u679C
+            }
+        }
+    }
+}
+`)],-1),o=[r];function c(h,d,i,p,u,x){return a(),t("div",null,o)}var l=n(s,[["render",c]]);export{_ as __pageData,l as default};
