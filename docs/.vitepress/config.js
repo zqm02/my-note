a7b387868fb5beb7f36f7cb9693f58722e5a17e9
zqm02 <zqm556194@gmail.com>
2022-07-10 21:52:56 +0800

    完成 条件分支：if和'?'

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -97,6 +97,10 @@ module.exports = {
           { text: "2.7类型转换", link: "/JS基础知识/2_7TypeCast.md" },
           { text: "2.8基础运算符", link: "/JS基础知识/2_8BaseOperator.md" },
           { text: "2.9值的比较", link: "/JS基础知识/2_9ValueComparison.md" },
+          {
+            text: "2.10条件分支",
+            link: "/JS基础知识/2_10ConditionalBranch.md",
+          },
         ],
       },
       {
